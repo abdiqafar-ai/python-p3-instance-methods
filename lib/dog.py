9a736ab0e5f8adb9535a07ef94833b06d61983aa
abdiqafar <iabdiqafar24@gmail.com>
2024-11-30 22:18:26 +0300

    commit

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,13 @@
 
 class Dog:
     # Class body goes here
+    def __init__(self):
+
+        pass
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
 
     #Instance method definition
     pass
